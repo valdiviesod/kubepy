0f9218dfead9c4e854246bdeacc12da5a9507585
v4ld1 <dzvaldiviesod@gmail.com>
2024-06-27 22:36:57 -0500

    chore: Update server_conf.sh script for Kubernetes installation and Docker configuration

--- server_conf.sh
+++ server_conf.sh
@@ -1,41 +1,31 @@
-# Instalacion y configuracion de un servidor kubernetes en Debian 12
-sudo apt-get update && sudo apt-get upgrade -y
-sudo swapoff -a
-sudo sed -i '/ swap / s/^/#/' /etc/fstab
-sudo apt-get install -y apt-transport-https ca-certificates curl gpg containerd git
-# Key repo
+# Instalacion y configuracion del servidor kubernetes en Ubuntu Server 24.04
+sudo snap install microk8s --classic
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-# Añadir repositorio Version 1.30
-# Para cambiar de release modificar la version en /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudo apt-get install -y kubectl kubelet kubeadm
-sudo systemctl enable kubelet
-# Habilitar ipv4 forwarding
-sudo sysctl -w net.ipv4.ip_forward=1
-echo "net.ipv4.ip_forward=1" | sudo tee -a /etc/sysctl.conf
-sudo sysctl -p
-# Configuracion de red interna
-sudo kubeadm init --pod-network-cidr=<your-pod-network-cidr>
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo apt-get install -y kubectl
+sudo snap alias microk8s.kubectl kubectl
+microk8s.kubectl config view --raw > $HOME/.kube/config
 
-# Configuracion de Docker
+# Configuracion de docker
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo systemctl enable docker
 
 # Configuracion de proyecto
 git clone https://github.com/valdiviesod/kubepy -b dev
