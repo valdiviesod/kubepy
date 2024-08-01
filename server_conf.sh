c524b2e4c390a502572d67d2ff9efde36bd3d945
Daniel Valdivieso <dzvaldiviesod@gmail.com>
2024-07-31 23:58:55 -0500

    chore: Update Kubernetes server configuration script and dependencies

--- server_conf.sh
+++ server_conf.sh
@@ -1,56 +1,120 @@
-# Instalacion y configuracion del servidor kubernetes en Ubuntu Server 24.04
-# Configurar dns con netplan previamente
-# Instalar microk8s en el proceso de configuracion o manualmente
-# sudo snap install microk8s --classic
+# Instalacion y configuracion del servidor kubernetes en Debian 12
 # git clone https://github.com/valdiviesod/kubepy -b dev
-sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
-# Reemplazar version del paquete segun la version actual
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
-sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
-sudo snap alias microk8s.kubectl kubectl
-mkdir $HOME/.kube
-touch mkdir $HOME/.kube/.config
-sudo microk8s.kubectl config view --raw > $HOME/.kube/config
+# Configurar red primero
+# Guia: https://reintech.io/blog/configuring-network-interfaces-debian-12
+# Ejecutar comandos como root
+
+# Configuracion de Kubernetes
+# Guia: https://www.bentasker.co.uk/posts/documentation/linux/building-a-k8s-cluster-on-debian-12-1-bookworm.html 
+# Puertos: TCP/6443: The port used by the Kubernetes API server
+# TCP/2379-2380: Used by the etcd API
+# TCP/10250: The Kubelet API
+# TCP/10257: Kube Controller Manager (see note below)
+# TCP/10259: Kube scheduler (see note below)
+# TCP/30000-32767: Nodeport services
+
+swapoff -a
+nano /etc/fstab # comment out the swap line
+
+# Bridge de interfaz de red
+cat <<EOF | tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-k8s.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+# Instalacion de containerd
+apt update
+apt install -y containerd
+
+containerd config default > /etc/containerd/config.toml
+
+nano /etc/containerd/config.toml
+
+# Cambiar a true: [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options] SystemdCgroup = true
+
+systemctl enable containerd
+systemctl restart containerd
+
+# Instalacion de Kubernetes
+apt install gnupg gnupg2 curl software-properties-common -y
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | gpg --dearmour -o /etc/apt/trusted.gpg.d/cgoogle.gpg
+apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+apt update
+apt install kubelet kubeadm kubectl -y
+
+apt-mark hold kubelet kubeadm kubectl
+
+kubeadm init --control-plane-endpoint=$HOSTNAME
+
+# Como usuario SIN PRIVILEGIOS
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+source <(kubectl completion bash)
+echo "source <(kubectl completion bash)" | tee -a ~/.bashrc
+
+# Calico para la administracion de red
+kubectl apply -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/calico.yaml
+
+# Configuracion de single node
+kubectl get nodes
+kubectl get nodes -o json | jq '.items[].spec.taints'
+kubectl taint node <nodename> node-role.kubernetes.io/control-plane:NoSchedule-
+
+# Probar
+kubectl get nodes -o json | jq '.items[].spec.taints'
+kubectl get pods -n kube-system
+
 
 # Configuracion de docker
 # Add Docker's official GPG key:
-sudo apt-get update
-sudo apt-get install ca-certificates curl -y
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+apt-get update
+apt-get install ca-certificates curl -y
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo systemctl enable docker
-sudo systemctl start docker
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # MySQL
-sudo apt install mysql-server -y
-sudo systemctl enable mysql
-sudo systemctl start mysql
-sudo mysql_secure_installation
-sudo systemctl restart mysqld
-# Configuracion de base de datos y permisos
-# https://docs.vultr.com/how-to-install-mysql-on-ubuntu-24-04
-
+# Ultima version repo mysql
+# https://dev.mysql.com/downloads/repo/apt/
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.30-1_all.deb
+dpkg -i mysql-apt-config_0.8.30-1_all.deb
+apt update
+apt install mysql-server -y
+systemctl enable mysql
+systemctl start mysql
+mysql_secure_installation
+# Configurar base de datos y permisos
 
 
 # Configuracion de proyecto (Pruebas)
-sudo apt install python3-pip python3-venv -y 
+apt install python3-pip python3-venv -y 
 cd kubepy
 mkdir .kube
 rsync -avh ~/.kube/ ~/kubepy/.kube/
 
-echo "Server Setup Complete"
+# Configurar firewall
+
 
