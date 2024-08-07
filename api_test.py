a7bb22cff621b430f6eca33a53bb576358b9fa65
Daniel Valdivieso <dzvaldiviesod@gmail.com>
2024-08-06 19:31:46 -0500

    chore: Update app.run() to bind to all network interfaces for debugging purposes

--- api_test.py
+++ api_test.py
@@ -153,4 +153,4 @@ def get_pod_terminal(pod_name):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
