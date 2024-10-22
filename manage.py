ab5e8b3d89ddf6c8d38cd48380d86d39d4c7d083
Your Name <you@example.com>
2024-10-21 23:01:08 -0500

    Refactor manage.py to remove unused imports and update the run command

--- manage.py
+++ manage.py
@@ -1,13 +1,8 @@
-from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from main import app
 from database.db import db
 
 migrate = Migrate(app, db)
-manager = Manager(app)
-
-# Agregar el comando de migración
-manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
