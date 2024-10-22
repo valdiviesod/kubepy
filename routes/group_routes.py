12ca5e1d8ee02f5e730263d76e23010359b8fb7c
Your Name <you@example.com>
2024-10-22 09:48:42 -0500

    Refactor group_routes.py to update route endpoints for creating, updating, and deleting groups

--- routes/group_routes.py
+++ routes/group_routes.py
@@ -3,8 +3,8 @@ from controller.group_controller import create_group, update_group, delete_group
 
 group_bp = Blueprint('group', __name__)
 
-group_bp.route('/create', methods=['POST'])(create_group)
-group_bp.route('/update/<int:group_id>', methods=['PUT'])(update_group)
-group_bp.route('/delete/<int:group_id>', methods=['DELETE'])(delete_group)
+group_bp.route('/create_group', methods=['POST'])(create_group)
+group_bp.route('/update_group/<int:group_id>', methods=['PUT'])(update_group)
+group_bp.route('/delete_group/<int:group_id>', methods=['DELETE'])(delete_group)
 group_bp.route('/all_groups', methods=['GET'])(get_all_groups)  
 group_bp.route('/group/<int:group_id>', methods=['GET'])(get_group) 
